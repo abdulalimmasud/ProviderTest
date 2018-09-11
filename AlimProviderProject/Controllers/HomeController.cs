ab6bab5e9a430ddf507987b015df3759649ff328
unknown <abdulalimmasud@gmail.com>
2018-09-11 06:05:16 +0600

    credentials

--- AlimProviderProject/Controllers/HomeController.cs
+++ AlimProviderProject/Controllers/HomeController.cs
@@ -7,18 +7,21 @@ using Microsoft.AspNetCore.Mvc;
 using AlimProviderProject.Models;
 using System.Net.Http;
 using Newtonsoft.Json;
-using Google.Apis.Gmail.v1;
-using Google.Apis.Auth.OAuth2;
 using System.IO;
 using System.Threading;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Util.Store;
 using Google.Apis.Services;
 using Google.Apis.Gmail.v1.Data;
+using System.Text;
 
 namespace AlimProviderProject.Controllers
 {
     public class HomeController : Controller
     {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "AlimProviderProject";
         public HomeController()
         {
             
@@ -128,36 +131,86 @@ namespace AlimProviderProject.Controllers
         {
             try
             {
-                UserCredential credential;
-                string[] Scopes = { GmailService.Scope.GmailReadonly };
-                using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+                ClientSecrets secrets = new ClientSecrets();
+                secrets.ClientId = GoogleApiCredential.GoogleClientId;
+                secrets.ClientSecret = GoogleApiCredential.GoogleClientSecret;
+                string credPath = System.Environment.GetFolderPath(
+                  System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
+                if (System.IO.File.Exists(credPath))
                 {
-                    string credPath = "token.json";
-                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                        GoogleClientSecrets.Load(stream).Secrets,
-                        Scopes,
-                        "user",
-                        CancellationToken.None,
-                        new FileDataStore(credPath, true)).Result;
+                    System.IO.File.Delete(credPath);
                 }
+                UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                      secrets,
+                      Scopes,
+                      "user",
+                      CancellationToken.None,
+                      new FileDataStore(credPath, true)).Result;
+
+                // Create Gmail API service. 
                 var service = new GmailService(new BaseClientService.Initializer()
                 {
                     HttpClientInitializer = credential,
-                    ApplicationName = "AlimProviderProject",
+                    ApplicationName = ApplicationName,
                 });
-                UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+                
+                var inboxlistRequest = service.Users.Messages.List("eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ5NjQ4ZTAzMmNhYzU4NDI0ZTBkMWE3YzAzMGEzMTk4ZDNmNDZhZGIifQ.eyJhenAiOiIxMDE0ODUxNzY0OTEtOGc2Ymlpb2c5bGN1YjVnb2ViZ2RuZW8xdDRqazRob20uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIxMDE0ODUxNzY0OTEtOGc2Ymlpb2c5bGN1YjVnb2ViZ2RuZW8xdDRqazRob20uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTQxNzY0NzkzOTgxMTQ2MTIxMTIiLCJlbWFpbCI6ImFsaW1jdTA4QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiQ016ZndmTkxlQ0VYMjFITWh4V2d4QSIsImV4cCI6MTUzNjYyNTE1NywiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwiaWF0IjoxNTM2NjIxNTU3fQ.Yd4xbe-xJdfpRpfC0EiCx3Na1eNIgU6uPCwdiqUhaWeCGUbrTGTwjS8h3lqBt_L65VhPUv1O6T18zI18010Si3iL0eX9RiDrqaG5UlDVlTWNMEOwDfmWwoBohv4lxmZNDBee8fTDFIO9rC8QG68i81t6It-M9dYzH6gB0DvaKcjcYlV4m6fvcVrM757aECIO_I3HpuVY2DwiWAeAw5CkYkL6bY70D0zilV-c_8SESRINhgioXHADwZ34Rx1DOBRLST-9mvLWS4G6oyij0gOH0LfJ4kBuDIHyYjKo0XVwOmWdWNRAVqtssWSckg4deRVR-KWTL1VyTzSr-5ekI2dNPg");
+                inboxlistRequest.LabelIds = "INBOX";
+                inboxlistRequest.IncludeSpamTrash = false;
+                var emailListResponse = inboxlistRequest.Execute();
 
-                // List labels.
-                IList<Label> labels = request.Execute().Labels;
-                if (labels != null && labels.Count > 0)
+                if (emailListResponse != null && emailListResponse.Messages != null)
                 {
-                    foreach (var labelItem in labels)
+                    //loop through each email and get what fields you want... 
+                    foreach (var email in emailListResponse.Messages)
                     {
-                        string name = labelItem.Name;
+
+                        var emailInfoRequest = service.Users.Messages.Get("your email id", email.Id);
+                        var emailInfoResponse = emailInfoRequest.Execute();
+
+                        if (emailInfoResponse != null)
+                        {
+                            String from = "";
+                            String date = "";
+                            String subject = "";
+
+                            //loop through the headers to get from,date,subject, body  
+                            foreach (var mParts in emailInfoResponse.Payload.Headers)
+                            {
+                                if (mParts.Name == "Date")
+                                {
+                                    date = mParts.Value;
+                                }
+                                else if (mParts.Name == "From")
+                                {
+                                    from = mParts.Value;
+                                }
+                                else if (mParts.Name == "Subject")
+                                {
+                                    subject = mParts.Value;
+                                }
+
+                                if (date != "" && from != "")
+                                {
+
+                                    foreach (MessagePart p in emailInfoResponse.Payload.Parts)
+                                    {
+                                        if (p.MimeType == "text/html")
+                                        {
+                                            byte[] data = FromBase64ForUrlString(p.Body.Data);
+                                            string decodedString = Encoding.UTF8.GetString(data);
+
+                                        }
+                                    }
+
+
+
+                                }
+
+                            }
+                        }
                     }
-                }
-                else
-                {
 
                 }
             }
@@ -165,6 +218,15 @@ namespace AlimProviderProject.Controllers
             {
 
             }
+        }
+        public static byte[] FromBase64ForUrlString(string base64ForUrlInput)
+        {
+            int padChars = (base64ForUrlInput.Length % 4) == 0 ? 0 : (4 - (base64ForUrlInput.Length % 4));
+            StringBuilder result = new StringBuilder(base64ForUrlInput, base64ForUrlInput.Length + padChars);
+            result.Append(String.Empty.PadRight(padChars, '='));
+            result.Replace('-', '+');
+            result.Replace('_', '/');
+            return Convert.FromBase64String(result.ToString());
         }
     }
 }
---
+++ AlimProviderProject/Models/AuthorizationCodeFlow.cs
@@ -0,0 +1,67 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Requests;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Util;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlimProviderProject.Models
+{
+    public class AuthorizationCodeFlow : IAuthorizationCodeFlow
+    {
+        public AuthorizationCodeFlow()
+        {
+
+        }
+        public IAccessMethod AccessMethod { get; }
+
+        public IClock Clock { get; }
+
+        public IDataStore DataStore { get; }
+
+        public AuthorizationCodeRequestUrl CreateAuthorizationCodeRequest(string redirectUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTokenAsync(string userId, CancellationToken taskCancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Task<TokenResponse> ExchangeCodeForTokenAsync(string userId, string code, string redirectUri, CancellationToken taskCancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenResponse> LoadTokenAsync(string userId, CancellationToken taskCancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenResponse> RefreshTokenAsync(string userId, string refreshToken, CancellationToken taskCancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeTokenAsync(string userId, string token, CancellationToken taskCancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldForceTokenRetrieval()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
